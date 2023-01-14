1d062933bb3f61c6dc8efa1fbf41bc8cdbc8e584
Jake Hansen <JakeMHansen95@gmail.com>
2023-01-14 14:26:09 -0600

    Setup new vim fugitive bind

--- lua/jake/plugin/vim-fugitive.lua
+++ lua/jake/plugin/vim-fugitive.lua
@@ -1,15 +1,16 @@
 return {
-   "tpope/vim-fugitive",
-   cmd = "Git",
-   init = function() 
-      require("which-key").register({
-         ["<leader>"] = {
-            g = {
-               name = "Git",
-               s = {vim.cmd.Git,"Status"}
+    "tpope/vim-fugitive",
+    cmd = "Git",
+    init = function()
+        require("which-key").register({
+            ["<leader>"] = {
+                g = {
+                    name = "Git",
+                    s = { vim.cmd.Git, "Status" },
+                   ["dt"] = { "<Cmd>Git difftool -d<CR>", "Difftool" }
+                },
             }
-         }
-      })
-   end,
-   cond = vim.g.vscode == nil
+        })
+    end,
+    cond = vim.g.vscode == nil,
 }
