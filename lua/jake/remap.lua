745b5c373847e09c9c1c9cc42f4b3f1d3c0c137c
Jake Hansen <JakeMHansen95@gmail.com>
2023-01-14 15:28:34 -0600

    Setup binds to change splt size

--- lua/jake/remap.lua
+++ lua/jake/remap.lua
@@ -3,22 +3,11 @@ local wk = require("which-key")
 -- Register leader mappings with description
 -- Normal mode registrations
 wk.register({
-    d = "Delete to blackhole",
     p = {
         name = "project",
         v = { "<cmd>Ex<cr>", "View" },
     },
-    s = {
-        r = { "Search and replace" },
-    },
-    y = "Yank to system clipboard",
-    Y = "Yank line to system clipboard",
 }, { prefix = "<leader>" })
--- Visual mode registrations
-wk.register({
-    y = "Yank to system clipboard",
-    d = "Delete to blackhole",
-}, { prefix = "<leader>", mode = "v" })
 
 -- X mode
 wk.register({
@@ -26,23 +15,20 @@ wk.register({
 }, { prefix = "<leader>", mode = "x", })
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-
-
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]],{desc = "Yank to system keyboard"})
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]],{desc = "Delete to blackhole"})
+vim.keymap.set("n", "<leader>Y", [["+Y]], {desc = "Yank line to system keyboard"})
 
 
 vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("i", "kj", "<Esc>")
 
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {desc = "Move highlighted line"})
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {desc = "Move highlighted line"})
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "J", "mzJ`z",{desc = "J"})
+vim.keymap.set("n", "<C-d>", "<C-d>zz",{desc="Move up one screen"})
+vim.keymap.set("n", "<C-u>", "<C-u>zz",{desc="Move down one screen"})
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 --
@@ -53,4 +39,9 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],{desc = "Search and replace"})
+
+vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
