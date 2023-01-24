615dbae5dfa53a84b05c72e6e1129b3fd6898fd4
Jake Hansen <JakeMHansen95@gmail.com>
2023-01-24 14:08:10 -0600

    Add bat to build neovim on windows

---
+++ make_neovim.bat
@@ -0,0 +1,10 @@
+:: Script to build lastest neovim for windows, the first argument is path to neovim git repo
+pushd %1
+git checkout master
+git pull
+cmake -S cmake.deps -B .deps -G Ninja -DCMAKE_BUILD_TYPE='Release'
+cmake --build .deps
+cmake -B build -G Ninja -DCMAKE_BUILD_TYPE='Release'  -DCMAKE_INSTALL_PREFIX="C:/Program Files/nvim"
+cmake --build build
+sudo ninja -C build install
+popd
