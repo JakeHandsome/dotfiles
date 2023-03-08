b747b9971fe6614fc087c33deaa9ab73cec49365
Jake Hansen <JakeMHansen95@gmail.com>
2023-03-07 21:16:44 -0600

    Underline mutable variables

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- LspSemanticTokens
+local st = vim.lsp.semantic_tokens
+vim.api.nvim_create_autocmd("LspTokenUpdate", {
+   callback = function(args)
+      local token = args.data.token
+      if token.modifiers.mutable then
+         st.highlight_token(token, args.buf, args.data.client_id, "UnderlineMutable")
+      end
+   end,
+})
+
+vim.api.nvim_set_hl(0, "UnderlineMutable", { underline = true })
