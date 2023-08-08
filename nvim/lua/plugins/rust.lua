25fe8c3fb938c829fabc74d86007ee5148690c42
Jake Hansen <JakeMHansen95@gmail.com>
2023-08-08 12:20:52 -0500

    Disable the inlay_hints from rust-tools

---
+++ nvim/lua/plugins/rust.lua
@@ -0,0 +1,13 @@
+return {
+   {
+      "simrat39/rust-tools.nvim",
+      ft = "rs",
+      opts = {
+         tools = {
+            inlay_hints = {
+               auto = false,
+            },
+         },
+      },
+   },
+}
