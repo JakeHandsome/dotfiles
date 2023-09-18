030e3f7c7a54318db2ab83b9c70cc8785f60aa11
Jake Hansen <HansenJake@JohnDeere.com>
2023-09-18 15:52:03 -0500

    Setup fnm

--- clink/clink_start.cmd
+++ clink/clink_start.cmd
@@ -1,13 +1,19 @@
 @echo off
-curl -u %USERNAME%:%GITHUB_AT% -X PATCH https://github.deere.com/api/v3/user -d "{\"name\":\"Jake Hansen\"}" -H "Accept: application/vnd.github.v3+json" | json name
-
 :: Enable multigrid in neovide by default
 set NEOVIDE_MULTIGRID=1
-
-:: Make cd always behave like cd /D
-doskey cd=cd /D $*
-:: use exa instead of ls
-doskey ls=exa $*
 doskey vim=nvim $*
 :: core utils
+where coreutils || echo "Installing core utils" && cargo binstall -y coreutils
 for  %%i in (arch b2sum b3sum base32 base64 basename basenc cat cksum comm cp csplit cut date dd df dir dircolors dirname du echo env expand expr factor false fmt hashsum hostname join link ln md5sum mkdir mktemp more mv nl nproc numfmt od paste pr printenv printf ptx pwd readlink realpath relpath rm rmdir runcon seq sha1sum sha224sum sha256sum sha3-224sum sha3-256sum sha3-384sum sha3-512sum sha384sum sha3sum sha512sum shake128sum shake256sum shred shuf sleep sort split sum sync tac tail tee test touch tr true truncate tsort uname unexpand uniq unlink vdir wc whoami yes) do doskey %%i=coreutils %%i $*
+
+:: use exa instead of ls
+where eza || echo "Installing eza" && cargo binstall -y eza
+doskey ls=eza $*
+
+where fnm || echo "Installing fnm" && cargo binstall -y fnm
+FOR /f "tokens=*" %%i IN ('fnm env --use-on-cd') DO CALL %%i
+doskey cd=%APPDATA%\fnm\cd.cmd /D $*
+:: Use fnm over nvm
+doskey nvm=fnm $*
+
+if %USERNAME%==jh49249 (curl -u %USERNAME%:%GITHUB_AT% -X PATCH https://github.deere.com/api/v3/user -d "{\"name\":\"Jake Hansen\"}" -H "Accept: application/vnd.github.v3+json" | json name) else (echo "not working")
