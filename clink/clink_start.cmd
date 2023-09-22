4f75a00af128ea0375c02431da3ad79385f09dd0
Jake Hansen <HansenJake@JohnDeere.com>
2023-09-22 15:49:41 -0500

    Hide console output

--- clink/clink_start.cmd
+++ clink/clink_start.cmd
@@ -3,14 +3,14 @@
 set NEOVIDE_MULTIGRID=1
 doskey vim=nvim $*
 :: core utils
-where coreutils || echo "Installing core utils" && cargo binstall -y coreutils
+where coreutils > nul 2>&1 || echo "Installing core utils" && cargo binstall -y coreutils
 for  %%i in (arch b2sum b3sum base32 base64 basename basenc cat cksum comm cp csplit cut date dd df dir dircolors dirname du echo env expand expr factor false fmt hashsum hostname join link ln md5sum mkdir mktemp more mv nl nproc numfmt od paste pr printenv printf ptx pwd readlink realpath relpath rm rmdir runcon seq sha1sum sha224sum sha256sum sha3-224sum sha3-256sum sha3-384sum sha3-512sum sha384sum sha3sum sha512sum shake128sum shake256sum shred shuf sleep sort split sum sync tac tail tee test touch tr true truncate tsort uname unexpand uniq unlink vdir wc whoami yes) do doskey %%i=coreutils %%i $*
 
 :: use exa instead of ls
-where eza || echo "Installing eza" && cargo binstall -y eza
+where eza  > nul 2>&1 || echo "Installing eza" && cargo binstall -y eza
 doskey ls=eza $*
 
-where fnm || echo "Installing fnm" && cargo binstall -y fnm
+where fnm  > nul 2>&1 || echo "Installing fnm" && cargo binstall -y fnm
 FOR /f "tokens=*" %%i IN ('fnm env --use-on-cd') DO CALL %%i
 doskey cd=%APPDATA%\fnm\cd.cmd /D $*
 :: Use fnm over nvm
