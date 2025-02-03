118a99957967255d674b70d6e79bd68a8019849f
Jake Hansen <HansenJake@JohnDeere.com>
2025-02-03 12:12:05 -0600

    Better pairs

---
+++ nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,12 @@
+return {
+   {
+      -- Better autopairs for rust lifetimes ex: &'
+      "windwp/nvim-autopairs",
+      event = "InsertEnter",
+      opts = {},
+   },
+   {
+      "echasnovski/mini.pairs",
+      enabled = false,
+   },
+}
