6812867ced21080bef576ba3c101b5915d75b266
Jake Hansen <HansenJake@JohnDeere.com>
2025-08-19 11:35:17 -0500

    Disable nvim-dap

--- nvim/lua/plugins/disable.lua
+++ nvim/lua/plugins/disable.lua
@@ -1,8 +1,8 @@
 return {
-  { "akinsho/bufferline.nvim", enabled = false },
+   { "akinsho/bufferline.nvim", enabled = false },
 
-  -- Disable when using NixOs
-  { "mason-lspconfig.nvim", enabled = not vim.g.NixOs },
-  { "mason-nvim-dap.nvim", enabled = not vim.g.NixOs },
-  { "mason.nvim", enabled = not vim.g.NixOs },
+   -- Disable when using NixOs
+   { "mason-lspconfig.nvim", enabled = not vim.g.NixOs },
+   { "mason-nvim-dap.nvim", enabled = false },
+   { "mason.nvim", enabled = not vim.g.NixOs },
 }
