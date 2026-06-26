09a565711e87900e413b216096aacd86a38b4844
Jake Hansen <34637103+JakeHandsome@users.noreply.github.com>
2026-06-26 13:51:55 -0500

    Upgrate grug-far behavior

--- nvim/lua/plugins/grug-far.lua
+++ nvim/lua/plugins/grug-far.lua
@@ -1,3 +1,25 @@
+-- Focus existing grug-far window if one is open, otherwise open new
+local function focus_or_open(opts)
+   local inst = require('grug-far.instances').get_instance()
+   if inst and inst:is_open() then
+      local win = vim.fn.bufwinid(inst._buf)
+      if win ~= -1 then
+         vim.api.nvim_set_current_win(win)
+         return
+      end
+   end
+   require('grug-far').open(opts)
+end
+
+-- Kill any existing grug-far instance, then open with new options
+local function replace_or_open(opts)
+   local inst = require('grug-far.instances').get_instance()
+   if inst then
+      inst:close()
+   end
+   require('grug-far').open(opts)
+end
+
 return {
    'MagicDuck/grug-far.nvim',
    cmd = { 'GrugFar', 'GrugFarWithin' },
@@ -5,18 +27,14 @@ return {
    keys = {
       {
          '<leader>go',
-         function()
-            require('grug-far').open({
-               transient = true,
-            })
-         end,
+         function() focus_or_open({ transient = true }) end,
          mode = { 'n', 'v' },
          desc = '[g]rug-far [o]pen',
       },
       {
          '<leader>gw',
          function()
-            require('grug-far').open({
+            replace_or_open({
                transient = true,
                prefills = { search = vim.fn.expand('<cword>') },
             })
@@ -27,7 +45,7 @@ return {
       {
          '<leader>gf',
          function()
-            require('grug-far').open({
+            replace_or_open({
                transient = true,
                prefills = { paths = vim.fn.expand('%') },
             })
